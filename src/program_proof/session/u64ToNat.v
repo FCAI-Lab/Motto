dcac75bce619422f05c9a2e5a3e4e6fb6b5e45e8
KiJeong-Lim <gijungdduk@naver.com>
2025-06-13 11:47:47 +0900

    .

--- src/program_proof/session/u64ToNat.v
+++ src/program_proof/session/u64ToNat.v
@@ -3,65 +3,6 @@ Require Import Perennial.program_proof.session.session_definitions.
 Reserved Infix ">>=" (left associativity, at level 90).
 Reserved Infix "=~=" (at level 70, no associativity).
 
-(** Section MONAD. *)
-
-#[universes(polymorphic=yes)]
-Class isMonad@{d c} (M : Type@{d} -> Type@{c}) : Type :=
-  { bind {A : Type@{d}} {B : Type@{d}} (m : M A) (k : A -> M B) : M B
-  ; pure {A : Type@{d}} : A -> M A
-  }.
-
-Infix ">>=" := bind.
-
-#[universes(polymorphic=yes)]
-Definition monad@{u v} {M : Type@{u} -> Type@{v}} {MONAD : isMonad@{u v} M} {A : Type@{u}} : Type@{v} :=
-  M A.
-
-Declare Scope monad_scope.
-Declare Custom Entry do_notation.
-
-Delimit Scope monad_scope with monad.
-Bind Scope monad_scope with monad.
-Open Scope monad_scope.
-
-Reserved Notation "'do' m" (m custom do_notation at level 10, at level 0, format "'do'  '//' m").
-Notation "'do' m" := m : monad_scope.
-Notation "'do' m" := (m : monad).
-
-Notation "x '<-' m1 ';' m2" := (m1 >>= fun x => m2) (in custom do_notation at level 0, x ident, m1 constr, m2 custom do_notation at level 10, format "x  '<-'  m1 ';' '//' m2").
-Notation "'let' x ':=' t ';' m" := (let x := t in m) (in custom do_notation at level 0, x pattern, t constr, m custom do_notation at level 10, format "'let'  x  ':='  t ';' '//' m").
-Notation "''' x '<-' m1 ';' m2" := (m1 >>= fun 'x => m2) (in custom do_notation at level 0, x pattern, m1 constr, m2 custom do_notation at level 10, format "''' x  '<-'  m1 ';' '//' m2").
-Notation "m1 ';' m2" := (m1 >>= fun _ => m2) (in custom do_notation at level 0, m1 constr, m2 custom do_notation at level 10, format "m1 ';' '//' m2").
-Notation "'ret' t" := (pure t) (in custom do_notation at level 10, t constr, format "'ret'  t").
-Notation "t" := t (in custom do_notation at level 0, t constr).
-
-#[global]
-Instance option_isMonad : isMonad option :=
-  { pure {A} := @Some A
-  ; bind {A} {B} (m : option A) (k : A -> option B) :=
-    match m with
-    | Some x => k x
-    | None => None
-    end
-  }.
-
-Notation "'fail'" := (None) (in custom do_notation at level 10, format "'fail'").
-
-(** End MONAD. *)
-
-(** Section OPTION. *)
-
-Fixpoint fold_left_option {A : Type} {B : Type} (f : A -> B -> option A) (xs : list B) (z : A) : option A :=
-  match xs with
-  | [] => do
-    ret z
-  | x :: xs' => do
-    'y <- f z x;
-    fold_left_option f xs' y
-  end.
-
-(** End OPTION. *)
-
 (** Section SIMILARITY. *)
 
 #[universes(template)]
@@ -86,23 +27,156 @@ Instance Similarity_list {A : Type} {A' : Type} (A_SIM : Similarity A A') : Simi
 
 (** End SIMILARITY. *)
 
+(** Section MONAD. *)
+
+#[universes(polymorphic=yes)]
+Class isMonad@{d c} (M : Type@{d} -> Type@{c}) : Type :=
+  { bind {A : Type@{d}} {B : Type@{d}} (m : M A) (k : A -> M B) : M B
+  ; pure {A : Type@{d}} : A -> M A
+  }.
+
+Infix ">>=" := bind.
+
+#[universes(polymorphic=yes)]
+Definition monad@{u v} {M : Type@{u} -> Type@{v}} {MONAD : isMonad@{u v} M} {A : Type@{u}} : Type@{v} :=
+  M A.
+
+Declare Scope monad_scope.
+Declare Custom Entry do_notation.
+
+Delimit Scope monad_scope with monad.
+Bind Scope monad_scope with monad.
+
+Reserved Notation "'do' m" (m custom do_notation at level 10, at level 0, format "'do'  '//' m").
+Notation "'do' m" := m : monad_scope.
+Notation "'do' m" := (m : monad).
+
+Notation "x '<-' m1 ';' m2" := (m1 >>= fun x => m2) (in custom do_notation at level 0, x ident, m1 constr, m2 custom do_notation at level 10, format "x  '<-'  m1 ';' '//' m2").
+Notation "'let' x ':=' t ';' m" := (let x := t in m) (in custom do_notation at level 0, x pattern, t constr, m custom do_notation at level 10, format "'let'  x  ':='  t ';' '//' m").
+Notation "''' x '<-' m1 ';' m2" := (m1 >>= fun 'x => m2) (in custom do_notation at level 0, x pattern, m1 constr, m2 custom do_notation at level 10, format "''' x  '<-'  m1 ';' '//' m2").
+Notation "m1 ';' m2" := (m1 >>= fun _ => m2) (in custom do_notation at level 0, m1 constr, m2 custom do_notation at level 10, format "m1 ';' '//' m2").
+Notation "'ret' t" := (pure t) (in custom do_notation at level 10, t constr, format "'ret'  t").
+Notation "t" := t (in custom do_notation at level 0, t constr).
+
+#[local] Open Scope monad_scope.
+
+#[universes(polymorphic=yes)]
+Definition identity@{u} (A : Type@{u}) : Type@{u} :=
+  A.
+
+#[global]
+Instance identity_isMonad : isMonad identity :=
+  { pure {A} (x : A) := x
+  ; bind {A} {B} (m : A) (k : A -> B) := k m
+  }.
+
+#[universes(polymorphic=yes)]
+Definition Err@{u} (A : Type@{u}) : Type@{u} :=
+  (bool * A)%type.
+
+#[global]
+Instance Err_isMonad : isMonad Err :=
+  { pure {A} (x : A) := (true, x)
+  ; bind {A} {B} (m : Err A) (k : A -> Err B) :=
+    let (b, y) := k m.2 in
+    (m.1 && b, y)
+  }.
+
+#[global]
+Instance option_isMonad : isMonad option :=
+  { pure {A} := @Some A
+  ; bind {A} {B} (m : option A) (k : A -> option B) :=
+    match m with
+    | Some x => k x
+    | None => None
+    end
+  }.
+
+Fixpoint fold_left' {M : Type -> Type} `{isMonad M} {A : Type} {B : Type} (f : A -> B -> M A) (xs : list B) (z : A) : M A :=
+  match xs with
+  | [] => do
+    ret z
+  | x :: xs' => do
+    'y <- f z x;
+    fold_left' f xs' y
+  end.
+
+Class isSuperMonad (M : Type -> Type) `{isMonad M} : Type :=
+  { plusplus : nat -> M nat
+  ; tryget {A : Type} : M A -> option A
+  ; tryget_pure {A : Type} (x : A)
+    : forall z : A, tryget (pure x) = Some z -> x = z
+  ; tryget_bind {A : Type} {B : Type} (m : M A) (k : A -> M B)
+    : forall z : B, tryget (bind m k) = Some z -> (exists x : A, tryget m = Some x /\ tryget (k x) = Some z)
+  }.
+
+Notation "t '++'" := (plusplus t) (in custom do_notation at level 1, t constr, format "t '++'").
+
+#[global, program]
+Instance Err_isSuperMonad : isSuperMonad Err :=
+  { plusplus (n : nat) := (Z.ltb (n + 1) CONSTANT, (n + 1)%nat)
+  ; tryget {A} (m : Err A) := if m.1 then Some m.2 else None
+  }.
+Next Obligation.
+  intros. simpl in *. congruence.
+Qed.
+Next Obligation.
+  intros. cbn in *. destruct m as [[ | ] x]; cbn in *.
+  - exists x. split; trivial. destruct (k x) as [[ | ] y]; cbn in *; try congruence.
+  - destruct (k x) as [[ | ] y]; cbn in *; try congruence.
+Qed.
+
+#[global, program]
+Instance option_isSuperMonad : isSuperMonad option :=
+  { plusplus (n : nat) := if Z.ltb (n + 1) CONSTANT then Some (n + 1)%nat else None
+  ; tryget {A} (m : option A) := m
+  }.
+Next Obligation.
+  intros. unfold pure in *. simpl in *. congruence.
+Qed.
+Next Obligation.
+  intros. cbn in *. destruct m as [x | ]; simpl in *; try congruence.
+  exists x. split; trivial.
+Qed.
+
+(** End MONAD. *)
+
+#[global] Arguments bind {M} {isMonad} {A} {B} : simpl never.
+#[global] Arguments pure {M} {isMonad} {A} : simpl never.
+
 (** Section BASIC_CORRES. *)
 
-Definition param2func_corres {A : Type} {A' : Type} {B : Type} {B' : Type} {C : Type} {C' : Type} `{Similarity A A'} `{Similarity B B'} `{Similarity C C'} (f : A -> B -> C) (f' : A' -> B' -> option C') : Prop :=
-  forall a' : A', forall b' : B', forall c' : C', f' a' b' = Some c' ->
+Definition param0func_corres `{isSuperMonad M} {A : Type} {A' : Type} `{Similarity A A'} (f : A) (f' : M A') : Prop :=
+  forall a' : A', tryget f' = Some a' ->
+  exists a : A, a =~= a' /\ f = a.
+
+Definition param1func_corres `{isSuperMonad M} {A : Type} {A' : Type} {B : Type} {B' : Type} `{Similarity A A'} `{Similarity B B'} (f : A -> B) (f' : A' -> M B') : Prop :=
+  forall a' : A', forall b' : B', tryget (f' a') = Some b' ->
+  forall a : A, a =~= a' ->
+  exists b : B, b =~= b' /\ f a = b.
+
+Definition param2func_corres `{isSuperMonad M} {A : Type} {A' : Type} {B : Type} {B' : Type} {C : Type} {C' : Type} `{Similarity A A'} `{Similarity B B'} `{Similarity C C'} (f : A -> B -> C) (f' : A' -> B' -> M C') : Prop :=
+  forall a' : A', forall b' : B', forall c' : C', tryget (f' a' b') = Some c' ->
   forall a : A, a =~= a' ->
   forall b : B, b =~= b' ->
   exists c : C, c =~= c' /\ f a b = c.
 
-Lemma fold_left_corres {A : Type} {A' : Type} {B : Type} {B' : Type} `{Similarity A A'} `{Similarity B B'} (f : A -> B -> A) (f' : A' -> B' -> option A')
+Definition param3func_corres `{isSuperMonad M} {A : Type} {A' : Type} {B : Type} {B' : Type} {C : Type} {C' : Type} {D : Type} {D' : Type} `{Similarity A A'} `{Similarity B B'} `{Similarity C C'} `{Similarity D D'} (f : A -> B -> C -> D) (f' : A' -> B' -> C' -> M D') : Prop :=
+  forall a' : A', forall b' : B', forall c' : C', forall d' : D', tryget (f' a' b' c') = Some d' ->
+  forall a : A, a =~= a' ->
+  forall b : B, b =~= b' ->
+  forall c : C, c =~= c' ->
+  exists d : D, d =~= d' /\ f a b c = d.
+
+Lemma fold_left_corres `{isSuperMonad M} {A : Type} {A' : Type} {B : Type} {B' : Type} `{Similarity A A'} `{Similarity B B'} (f : A -> B -> A) (f' : A' -> B' -> M A')
   (f_corres : param2func_corres f f')
-  : param2func_corres (fold_left f) (fold_left_option f').
+  : param2func_corres (fold_left f) (fold_left' f').
 Proof.
   red. intros xs' z' y' H_OBS xs xs_corres z z_corres. revert z z' y' z_corres H_OBS.
   induction xs_corres as [ | x x' xs xs' x_corres xs_corres IH]; simpl; intros.
-  - exists z. split; trivial. congruence.
-  - destruct (f' z' x') as [_y' | ] eqn: H_y'; try congruence. eapply IH with (z' := _y'); trivial.
-    pose proof (f_corres z' x' _y' H_y' z z_corres x x_corres) as (? & H_y & <-); trivial.
+  - exists z. split; trivial. apply tryget_pure in H_OBS. congruence.
+  - apply tryget_bind in H_OBS. destruct H_OBS as (_y' & H_OBS & H_y'). eapply IH with (z' := _y'); trivial.
+    pose proof (f_corres z' x' _y' H_OBS z z_corres x x_corres) as (y & H_y & <-); trivial.
 Qed.
 
 (** End BASIC_CORRES. *)
